ab983e3f0b3978dd2c06c7c43afa6f71bebc367a
Felix Wong <fel_wong@outlook.com>
2023-05-30 13:05:50 -0400

    Initial commit
    first commit

---
+++ Yonder/Yonder/YonderApp.swift
@@ -0,0 +1,17 @@
+//
+//  YonderApp.swift
+//  Yonder
+//
+//  Created by Felix Wong on 2023-05-30.
+//
+
+import SwiftUI
+
+@main
+struct YonderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
