ee60fdb06e2a0a6c14b5492cf1be8c77541052dd
Felix Wong <fel_wong@outlook.com>
2023-07-18 17:05:06 -0400

    delete trip button and redirect users to profileview after adding and deleting trip
    Delete trip available and finally i am able to guide the user back to the profileView

--- Yonder/Yonder/Core/Trip/TripView.swift
+++ Yonder/Yonder/Core/Trip/TripView.swift
@@ -6,36 +6,73 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseFirestoreSwift
 
 struct TripView: View {
     let tripForm: TripForm
-
+    @State private var tripDeleted = false
+    @State private var showConfirmationAlert = false
+    
     var body: some View {
         VStack(spacing: 16) {
             Text("Trip Details")
                 .font(.title)
                 .fontWeight(.bold)
-
+            
             Text("Trip Name: \(tripForm.tripName)")
                 .font(.headline)
-
+            
             Text("Arrival Date: \(formatDate(tripForm.arrivalDate))")
                 .font(.subheadline)
-
+            
             Text("Leaving Date: \(formatDate(tripForm.leavingDate))")
                 .font(.subheadline)
-
+            
             // Additional trip details
-
+            
             Spacer()
+            
+            Button {
+                showConfirmationAlert = true 
+            } label: {
+                Text("Delete Trip")
+                    .foregroundColor(.red)
+            }
         }
         .padding()
         .navigationTitle("Trip Details") // Set the navigation title
+        .background(
+            NavigationStack {
+                NavigationLink(
+                    destination: ProfileView(), // Navigate to ProfileView when the trip is deleted
+                    isActive: $tripDeleted, // Use tripDeleted to control the navigation
+                    label: { EmptyView() }
+                )
+            }
+        )
+        .alert(isPresented: $showConfirmationAlert) {
+                    Alert(
+                        title: Text("Delete Trip"),
+                        message: Text("Are you sure you want to delete this trip?"),
+                        primaryButton: .cancel(),
+                        secondaryButton: .destructive(Text("Delete"), action: deleteTrip)
+                    )
+                }
     }
-
-    private func formatDate(_ date: Date) -> String {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .medium
-        return formatter.string(from: date)
+    
+    private func deleteTrip() {
+        // Implement the code to delete the tripForm from Firebase or your data source
+        // For example, you can use Firestore to delete the document corresponding to the tripForm
+        
+        let db = Firestore.firestore()
+        db.collection("tripForms").document(tripForm.id).delete { error in
+            if let error = error {
+                print("Error deleting trip: \(error.localizedDescription)")
+            } else {
+                // Trip deletion successful
+                tripDeleted = true // Set the tripDeleted state to true to navigate back to ProfileView
+            }
+        }
     }
 }
